425b90a454455a415ec201033645eae7af7c3406
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-09-01 16:23:26 -0500

    Modernize Python packaging and tooling

--- openff/amber_ff_ports/__init__.py
+++ openff/amber_ff_ports/__init__.py
@@ -1,10 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from openff.amber_ff_ports.amber_ff_ports import get_forcefield_dirs_paths
 
-from ._version import get_versions
+__all__ = ("get_forcefield_dirs_paths",)
 
-__all__ = (get_forcefield_dirs_paths,)
+try:
+    __version__ = version("openff-amber-ff-ports")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
 
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+__git_revision__ = None
